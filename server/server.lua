015a0d58c2017dc4109d5c8ef7f47d1536fa8590
Dejmir <48758186+Dejmir@users.noreply.github.com>
2021-04-02 14:45:24 +0200

    Delete server directory

--- server/server.lua
+++
@@ -1,9 +0,0 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterUsableItem('fajnyheist_contact', function(playerId)
-	local xPlayer = ESX.GetPlayerFromId(playerId)
-	TriggerClientEvent('esx_extraitems:fajnyheist_contact', playerId)
-	TriggerClientEvent("military_heist:contact:start", playerId)
-end)
